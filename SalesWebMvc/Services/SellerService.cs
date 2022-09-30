d47ac9b14128e21a724a90f16ad8ad5ce6be9754
Leandro Vieira <leandro.v.pordeus@gmail.com>
2022-09-29 23:05:50 -0300

    Criando o Delete

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
@@ -26,5 +27,21 @@ namespace SalesWebMvc.Services
             _context.SaveChanges();
 
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+          
+
+        }
+
+      
     }
 }
